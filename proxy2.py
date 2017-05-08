3ba83bdc35ccfd170ba8c9b209fdd67ed21b68e8
LunacyZeus <LunacyZeus@users.noreply.github.com>
2017-05-08 22:58:19 +0800

    Update proxy2.py

--- proxy2.py
+++ proxy2.py
@@ -382,5 +382,30 @@ def test(HandlerClass=ProxyRequestHandler, ServerClass=ThreadingHTTPServer, prot
     httpd.serve_forever()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":  
+    # do the UNIX double-fork magic, see Stevens' "Advanced   
+    # Programming in the UNIX Environment" for details (ISBN 0201563177)  
+    try:   
+        pid = os.fork()   
+        if pid > 0:  
+            # exit first parent  
+            sys.exit(0)   
+    except OSError, e:   
+        print >>sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror)   
+        sys.exit(1)  
+    # decouple from parent environment  
+    os.chdir("/")   
+    os.setsid()   
+    os.umask(0)   
+    # do second fork  
+    try:   
+        pid = os.fork()   
+        if pid > 0:  
+            # exit from second parent, print eventual PID before  
+            print "Daemon PID %d" % pid   
+            sys.exit(0)   
+    except OSError, e:   
+        print >>sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror)   
+        sys.exit(1)
+    # start the daemon main loop  
     test()
